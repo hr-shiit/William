82fbff31f1bd553266bc6270b522865b307459dd
hr-shiit <01001000.srivastav@gmail.com>
2025-04-30 01:06:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+HORIZON_URL = "https://horizon-testnet.stellar.org"
+NETWORK_PASSPHRASE = "Test SDF Network ; September 2015"
+
+# Replace these with your actual testnet keys
+SOURCE_SECRET = "PRIVATE KEY"  # Your testnet secret key
+MONITOR_ACCOUNT_ID = "PUBLIC KEY"  # Your testnet public key
+DESTINATION_ACCOUNT_ID = ""  # Can leave empty for manual inputs
