313ecced202d3d108e160d44c7333d778192f3d1
hr-shiit <01001000.srivastav@gmail.com>
2025-04-30 01:08:50 +0530

    Create payments.py

---
+++ payments.py
@@ -0,0 +1,31 @@
+from stellar_sdk import Server, Keypair, TransactionBuilder, Network, Asset
+from stellar_sdk.operation import Payment
+from config import HORIZON_URL, NETWORK_PASSPHRASE
+
+server = Server(HORIZON_URL)
+
+def send_payment(source_secret, destination_public, amount):
+    source_keypair = Keypair.from_secret(source_secret)
+    source_account = server.load_account(source_keypair.public_key)
+
+    transaction = (
+        TransactionBuilder(
+            source_account=source_account,
+            network_passphrase=NETWORK_PASSPHRASE,
+            base_fee=100,
+        )
+        .append_operation(
+            Payment(
+                destination=destination_public,
+                asset=Asset.native(),
+                amount=str(amount)
+            )
+        )
+        .set_timeout(30)
+        .build()
+    )
+
+    transaction.sign(source_keypair)
+    response = server.submit_transaction(transaction)
+    return response
+    
